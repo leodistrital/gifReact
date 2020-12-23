c24058984e47ca72c7014241ffa037ed21fbda92
Leonardo Cortes <leodistrital@hotmail.com>
2020-12-23 15:31:42 -0500

    docs agregado

---
+++ docs/precache-manifest.f736b867fc5a4a48cd805e654d95ba0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87f90c49722b6c09749a",
+    "url": "/static/css/main.eb08b39c.chunk.css"
+  },
+  {
+    "revision": "87f90c49722b6c09749a",
+    "url": "/static/js/main.472500e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59244059b6242dd5c7c2",
+    "url": "/static/js/2.697c2ea7.chunk.js"
+  },
+  {
+    "revision": "891d2a6b2d3954bf384a824d07b79a5d",
+    "url": "/index.html"
+  }
+];
